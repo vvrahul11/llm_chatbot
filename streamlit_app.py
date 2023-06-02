247a46b316b01a2ab682fb2a03603c32a8cd12d9
Rahul Veettil <rahul.epfl@gmail.com>
2023-06-03 00:39:49 +0200

    feat: Added falcon and gpt modules

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,18 @@
 import streamlit as st
 from streamlit_chat import message
-from utils import *
+from gpt import *
 
 
 st.set_page_config(
-    page_title="Falcon App"
+    page_title="LLM Chatbot"
 )
+st.header("Biomedical LLM Chatbot")
 st.sidebar.header("Instructions")
 st.sidebar.info(
     '''**This is a web application that allows you to interact with Falcon 7B
     parameter opensource model. Enter a query in the text box and press enter
     to receive a **response**'''
     )
-st.header("Streamlit Chat using Falcon 7B")
-st.markdown("[Github](https://github.com/vvrahul11)")
-
 
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
